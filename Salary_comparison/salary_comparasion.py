449b0483d472b1ec8f4c8ff274830b8dd1bb2e5a
Fernando Chavez <48636616+cholu6768@users.noreply.github.com>
2020-07-26 16:46:47 +0200

    Add files via upload

---
+++ Salary_comparison/salary_comparasion.py
@@ -0,0 +1,24 @@
+#Function that loads the name, salary of an employee and saves it in a list
+def load_employee():
+  num_employee = int(input("How many employees would you like upload: "))
+  employees = []
+  n = 1
+  while(num_employee >= n):
+    name = input("Insert the name of the employee: ")
+    salary = int(input("Insert the salary: "))
+    employee = (name,salary)
+    employees.append(employee)
+    n += 1
+  return employees
+
+#Function that checks the employee with the highest salary
+def biggest_salary(lst):
+  salary = 0
+  for n in lst:
+    if n[1] > salary:
+      salary = n[1]
+      nanme = n[0]
+  print(f"El empleado con mayor sueldo es: {name}")
+
+
+biggest_salary(load_employee())
